b08475ccbdd80bd354044953614a471d2f25bb8a
Artiom Rumiancev <artasr@gmail.com>
2022-06-20 10:41:13 +0300

    feat: created interactive-editable-post-card

--- src/pages/edit-posts-page/interactive-editable-post-card.tsx
+++ src/pages/edit-posts-page/interactive-editable-post-card.tsx
@@ -1,46 +1,21 @@
 import {
   Button,
   Card,
-  CardActions,
-  CardContent,
   CardMedia,
   Typography,
-  Container,
   Box,
 } from '@mui/material';
 import React from 'react';
-import { Post } from '../types';
+import { Post } from '../../types';
 
 export type PostCardProps = Omit<Post, 'id'>;
-// export type PostCardProps = Post;
 
-const PetCard: React.FC<PostCardProps> = ({
+const InteractiveEditablePostCard: React.FC<PostCardProps> = ({
   petName, author, description, picURL,
 }) => (
 
-  <Card sx={{ maxWidth: 345 }}>
-    <Typography>{author}</Typography>
-    <CardMedia
-      component="img"
-      height="140"
-      image={picURL}
-      alt="picture of your potential pet"
-    />
-    <CardContent>
-      <Typography gutterBottom variant="h5" component="div">
-        {petName}
-      </Typography>
-      <Typography variant="body2" color="text.secondary">
-        {/* Lizards are a widespread group of squamate reptiles, with over 6,000
-        species, ranging across all continents except Antarctica. */}
-        {description}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small">Share</Button>
-      <Button size="small">Learn More</Button>
-    </CardActions>
-    {/* ------------- -----------------------*/}
+  <Card sx={{ maxWidth: 445 }}>
+
     <Card>
       <CardMedia
         component="img"
@@ -104,29 +79,4 @@ const PetCard: React.FC<PostCardProps> = ({
 
 );
 
-// const PetCard: React.FC<PostCardProps> = (props) => {
-//   const {
-//     petName, description, picURL, createdAt, updatedAt,
-//   } = props;
-
-//   return (
-//     <Card>
-//       <Container>
-
-//         <h3>{petName}</h3>
-
-//         <h3>{description}</h3>
-//         <img
-//           src={`${picURL}`}
-//           alt=""
-//           loading="lazy"
-//         />
-//         <h3>{new Date(createdAt).toLocaleString()}</h3>
-//         <h3>{new Date(updatedAt).toLocaleString()}</h3>
-
-//       </Container>
-//     </Card>
-//   );
-// };
-
-export default PetCard;
+export default InteractiveEditablePostCard;
