f2ada045db243e29de9966e67ae28273b6bf4472
Artiom Rumiancev <artasr@gmail.com>
2022-06-20 08:19:20 +0300

    feat: added posts: postsReducer to mainReducer

--- src/store/main-reducer.ts
+++ src/store/main-reducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import authReducer from './features/auth/auth-reducer';
 import navigationReducer from './features/navigation/navigation-reducer';
+import postsReducer from './features/post/post-reducer';
 
 const mainReducer = combineReducers({
   auth: authReducer,
   navigation: navigationReducer,
+  posts: postsReducer,
 });
 
 export default mainReducer;
